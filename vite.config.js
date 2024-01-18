d2fc3c474e94dd5676842dcc913f4141b1c70de2
wswoodruff <williamswoodruff@gmail.com>
2024-01-17 20:50:14 -0500

    Copy folders to dist

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+export default {
+    plugins: [
+        viteStaticCopy({
+            targets: [
+                { src: 'images', dest: '.' },
+                { src: 'models', dest: '.' },
+                { src: 'svgs', dest: '.' },
+                { src: 'videos', dest: '.' }
+            ]
+        })
+    ]
+}
