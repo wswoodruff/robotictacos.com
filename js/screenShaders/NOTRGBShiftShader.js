e4e7ed54e03e58fbef1ab367fcb3c22a85760342
tripdragon <nickitdesign@gmail.com>
2024-05-06 13:23:53 -0400

    annoying

--- js/screenShaders/NOTRGBShiftShader.js
+++ js/screenShaders/NOTRGBShiftShader.js
@@ -28,6 +28,8 @@ const NOTRGBShiftShader = {
   // main thing to tweak
 	fragmentShader: /* glsl */`
 
+  #define PI 3.14159265359
+  #define TWO_PI 6.28318530718
   
   varying vec2 vUv;
   uniform vec2 resolution;
@@ -45,6 +47,35 @@ vec3 ring(vec2 pos, float radius, float width){
   
   return color2 - color3;
 }
+
+
+// Reference to
+// http://thndl.com/square-shaped-shaders.html
+
+vec3 shape(int pointsN, vec2 st, float size){
+      // Number of sides of your shape
+  int N = pointsN;
+  
+  size = clamp(size, 0.01, 10.);
+
+  // Angle and radius from the current pixel
+  float a = atan(st.x,st.y)+PI;
+  float r = TWO_PI/float(N);
+
+  // Shaping function that modulate the distance
+  float d = cos(floor(0.500+a/r)*r-a)*length(st) / size;
+
+  vec3 color = vec3(1.0-smoothstep(.4,.405,d));
+  // color = vec3(d);
+    return color;
+}
+
+vec3 outlineShape(int pointsN, vec2 st, float radius, float width){
+    vec3 color1 = shape(pointsN, st, radius);
+    vec3 color2 = shape(pointsN, st, radius-width);
+    return color1 - color2;
+}
+
 
 float map(float value, float min1, float max1, float min2, float max2) {
   return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
@@ -102,13 +133,13 @@ float map(float value, float min1, float max1, float min2, float max2) {
     
     vec3 color2 = ring(pos, 1.9, 0.05);
     
-    gl_FragColor += vec4( color2, 1.0);
+    // gl_FragColor += vec4( color2, 1.0);
     
     vec3 color3 = ring(pos, 2.2, 0.02);
-    gl_FragColor += vec4( color3, 1.0);
+    // gl_FragColor += vec4( color3, 1.0);
     
     vec3 color4 = ring(pos, 2.8, 0.01);
-    gl_FragColor += vec4( color4, 1.0);
+    // gl_FragColor += vec4( color4, 1.0);
 
     // float sw = cos(u_time*2.)*2.+1.4;
     float sw = map( cos(u_time *4.0), -1.0,1.0,1.9,3.0  );
@@ -120,6 +151,21 @@ float map(float value, float min1, float max1, float min2, float max2) {
     vec3 color6 = ring(pos, sw2, 0.01);
     gl_FragColor += vec4( color6, 1.0);
     
+    
+    vec2 pos2 = pos;
+    pos2.y *= -1.;
+    	// gl_FragColor.rgb += outlineShape(3, pos2, 1.160, 0.02);
+    
+    // gl_FragColor.rgb += outlineShape(3, pos2, 1.060, 0.02);
+    
+    
+    for (int i = 1; i < 5; i++) {
+      // gl_FragColor.rgb += outlineShape(3, pos2, 1.060-(float(i)*0.2), 0.02);
+      // gl_FragColor.rgb += outlineShape(3, pos2, sin(1.060-(float(i)*0.2)), 0.02);
+      gl_FragColor.rgb += outlineShape(3, pos2, sin(2.060-(float(i)*0.2*u_time)), 0.02);
+      gl_FragColor.rgb += outlineShape(3, pos2, sin(2.060-(float(i)*0.2*u_time))*9., 0.02);
+      // gl_FragColor.r = float(i);
+    }
     
   }`
 
