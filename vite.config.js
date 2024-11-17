16c4b7c13cc1f4e9aaf01ac0661ea2bd94f32abd
tripdragon <nickitdesign@gmail.com>
2024-11-17 16:19:39 -0500

    ^&#&22

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import { viteStaticCopy as Copy } from 'vite-plugin-static-copy'
 import path from 'path';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
+import { readdirSync } from 'fs';
+import { resolve } from 'path';
 
 export default {
     plugins: [
@@ -19,17 +21,22 @@ export default {
     ],
     build: {
       rollupOptions: {
-        input: {
-          main: './index.html',
-          main222: './index222.html',
-          main222fun: './index222fun.html',
-          tacos: './indextacos.html',
-          rock: './indexrock.html',
-          dogskate: './index_dogskate.html',
-          jellysealife: './index_jellysealife01.html'
-          // ...
-          // List all files you want in your build
-        }
+        // input: {
+        //   main: './index.html',
+        //   main222: './index222.html',
+        //   main222fun: './index222fun.html',
+        //   tacos: './indextacos.html',
+        //   rock: './indexrock.html',
+        //   dogskate: './index_dogskate.html',
+        //   jellysealife: './index_jellysealife01.html'
+        //   // ...
+        //   // List all files you want in your build
+        // }
+        input: Object.fromEntries(
+                readdirSync('./') // Read the root directory
+                  .filter((file) => file.startsWith('index') && file.endsWith('.html')) // Filter files
+                  .map((file) => [file.replace(/\.html$/, ''), resolve(__dirname, file)]) // Create key-value pairs
+              )
       }
     },
     resolve : {
