a90233dc09f4f3747c3645841266dc3991405b4a
tripdragon <nickitdesign@gmail.com>
2024-11-20 21:52:04 -0500

    adding videos

--- components/bios/bio-nick.svelte
+++ components/bios/bio-nick.svelte
@@ -170,6 +170,11 @@
       <li>Blender 3D</li>
       <li>Vive</li>
     </ul>
+    <video poster="./videos/tacos_in2.gif" playsinline muted="true" autoplay="true" loop class="_featured-video">
+      <source src="/videos/vrkelp.mp4" type="video/mp4" />
+      Your browser does not support the video tag guesses.
+    </video>
+
   </section>
   <aside>
     <video poster="./videos/tacos_in2.gif" playsinline muted="true" autoplay="true" loop class="featured-video">
@@ -247,4 +252,240 @@
     </video>
   </aside>
 
+</article>
+
+
+
+
+
+
+
+<article>
+  <section>
+    <h3>Opus Logica</h3>
+    <p>Opus Logica. Contractor for promotional videos to name a few</p>
+    <!-- <p>Straight Talk Google VR. Google Cardboard android phone VR powered game for tradeshow.</p> -->
+    <ul class="copy-list">
+      <li>Website contractor tasks</li>
+      <li>Mock app build</li>
+      <li>Promotion videos for project Gistify</li>
+      <li>Promotion videos Music app</li>
+    </ul>
+    <ul class="tags-noise">
+      <li>javascript</li>
+      <li>After Effects</li>
+    </ul>
+  </section>
+</article>
+
+
+
+<article>
+  <section>
+    <h3>Alakazam</h3>
+    <!-- <p>Straight Talk Google VR. Google Cardboard android phone VR powered game for tradeshow.</p> -->
+    <p>Alakazam inc. Contractor for Web game to speed up various shaders and optimize visual elements like fluffing up trees, building flowing reflective water effect, building custom mixing shaders for opening cinematic</p>
+    <ul class="copy-list">
+      <li>Unity network editor 1 : Build unity based web editor parts</li>
+    </ul>
+    <ul class="tags-noise">
+      <li>javascript</li>
+      <li>threejs</li>
+    </ul>
+  </section>
+</article>
+
+<article>
+  <section>
+    <h3>Mozilla Hubs</h3>
+    <!-- <p>Straight Talk Google VR. Google Cardboard android phone VR powered game for tradeshow.</p> -->
+    <p>Mozilla Hubs. Alakazam inc. Sub project</p>
+    <ul class="copy-list">
+      <li>Work out networked physics plugin</li>
+      <li>Work out internals of code base</li>
+      <li>Build fully custom 3d model and Avatar editor exporter for hubs internal</li>
+    </ul>
+    <ul class="tags-noise">
+      <li>javascript</li>
+      <li>threejs</li>
+    </ul>
+  </section>
+  <aside>
+    <video poster="./videos/tacos_in2.gif" playsinline id="creative-iphone" muted="true" autoplay="true" loop class="featured-video">
+      <source src="/videos/arshoe1.mp4" type="video/mp4" />
+        Your browser does not support the video tag guesses.
+    </video>
+
+  </aside>
+</article>
+
+<article>
+  <section>
+    <h3>SwisherSweets AR</h3>
+    <!-- <p>Straight Talk Google VR. Google Cardboard android phone VR powered game for tradeshow.</p> -->
+    <p>SwisherSweets AR. Alakazam inc. Sub project</p>
+    <ul class="copy-list">
+      <li>Build 8th Wall mobile ar app to showcase brands products at trade show</li>
+      <li>Web interface code, AR swapping, Canvas map build</li>
+    </ul>
+    <ul class="tags-noise">
+      <li>javascript</li>
+      <li>threejs</li>
+      <li>8thWall</li>
+    </ul>
+  </section>
+</article>
+
+<article>
+  <section>
+    <h3>Porche AR</h3>
+    <!-- <p>Straight Talk Google VR. Google Cardboard android phone VR powered game for tradeshow.</p> -->
+    <p>Porche AR. Alakazam inc. Sub project</p>
+    <ul class="copy-list">
+      <li>Build logic and web app via 8th Wall to load and swap between provided dancers various mixes</li>
+    </ul>
+    <ul class="tags-noise">
+      <li>javascript</li>
+      <li>threejs</li>
+      <li>8thWall</li>
+    </ul>
+  </section>
+</article>
+
+<article>
+  <section>
+    <h3>PoodleShark</h3>
+    <!-- <p>Straight Talk Google VR. Google Cardboard android phone VR powered game for tradeshow.</p> -->
+    <p>PoodleShark. Unity game Snail element</p>
+    <ul class="copy-list">
+      <li>Built AI snail walking logic component for mobile game developer</li>
+      <li>Was featured in a Kickstarter</li>
+      <li>Physics chain logic</li>
+    </ul>
+    <ul class="tags-noise">
+      <li>Unity 3D</li>
+      <li>Maya3D</li>
+    </ul>
+  </section>
+</article>
+
+
+<article>
+  <section>
+    <h3>Tribeca Squid Lights</h3>
+    <!-- <p>Straight Talk Google VR. Google Cardboard android phone VR powered game for tradeshow.</p> -->
+
+    <ul class="copy-list">
+      <li>LED programming for a large set piece held in New York's Tribeca Film Festival interactive exhibit floor</li>
+    </ul>
+    <!-- just visual noise -->
+    <ul class="tags-noise">
+      <li>Arduino</li>
+    </ul>
+  </section>
+  <!-- <aside>
+    <video poster="./videos/tacos_in2.gif" playsinline muted="true" autoplay="true" loop class="featured-video">
+      <source src="/videos/straighttalk.mp4" type="video/mp4" />
+        Your browser does not support the video tag guesses.
+    </video>
+  </aside> -->
+
+</article>
+
+
+
+<article>
+  <section>
+    <h3>ZAL.ai</h3>
+    <!-- <p>Straight Talk Google VR. Google Cardboard android phone VR powered game for tradeshow.</p> -->
+    <p>ZAL.ai assist with AI bot</p>
+    <ul class="copy-list">
+      <li>Rework some AI logic streams</li>
+      <li>Backend/Frontend dev for various UI edits and debugging some AI</li>
+      <li>Webhook workflow for Hubspot to AI and back</li>
+    </ul>
+    <ul class="tags-noise">
+      <li>javascript</li>
+      <li>ChatGPT</li>
+      <li>python</li>
+      <li>docker</li>
+    </ul>
+  </section>
+</article>
+
+<article>
+  <section>
+    <h3>Lone Wolf Documentary Group</h3>
+    <!-- <p>Straight Talk Google VR. Google Cardboard android phone VR powered game for tradeshow.</p> -->
+    <p>Lone Wolf Documentary Group. Staff graphics</p>
+    <ul class="copy-list">
+      <li>Staff, creative for building motion cuts of archival effects featured on History Channel, NOVA/PBS, Discovery Channel</li>
+      <li>Built internal Network file share application</li>
+    </ul>
+    <ul class="tags-noise">
+      <li>After Effects</li>
+      <li>Rails</li>
+      <li>Photoshop</li>
+    </ul>
+  </section>
+</article>
+
+
+
+
+
+<article>
+  <section>
+    <h3>Others</h3>
+    <!-- <p>Straight Talk Google VR. Google Cardboard android phone VR powered game for tradeshow.</p> -->
+    <video poster="./videos/tacos_in2.gif" playsinline muted="true" autoplay="true" loop class="featured-video">
+      <source src="/videos/atractor1.mp4" type="video/mp4" />
+        Your browser does not support the video tag guesses.
+    </video>
+    <video poster="./videos/tacos_in2.gif" playsinline muted="true" autoplay="true" loop class="featured-video">
+      <source src="/videos/ball1.mp4" type="video/mp4" />
+        Your browser does not support the video tag guesses.
+    </video>
+    <video poster="./videos/tacos_in2.gif" playsinline muted="true" autoplay="true" loop class="featured-video">
+      <source src="/videos/arbrid1b.mp4" type="video/mp4" />
+        Your browser does not support the video tag guesses.
+    </video>
+    <video poster="./videos/tacos_in2.gif" playsinline muted="true" autoplay="true" loop class="featured-video">
+      <source src="/videos/candystar.mp4" type="video/mp4" />
+        Your browser does not support the video tag guesses.
+    </video>
+    <video poster="./videos/tacos_in2.gif" playsinline muted="true" autoplay="true" loop class="featured-video">
+      <source src="/videos/kindaai.mp4" type="video/mp4" />
+        Your browser does not support the video tag guesses.
+    </video>
+    <video poster="./videos/tacos_in2.gif" playsinline muted="true" autoplay="true" loop class="featured-video">
+      <source src="/videos/infinigame1.mp4" type="video/mp4" />
+        Your browser does not support the video tag guesses.
+    </video>
+    <video poster="./videos/tacos_in2.gif" playsinline muted="true" autoplay="true" loop class="featured-video">
+      <source src="/videos/gamegrid1.mp4" type="video/mp4" />
+        Your browser does not support the video tag guesses.
+    </video>
+    <video poster="./videos/tacos_in2.gif" playsinline muted="true" autoplay="true" loop class="featured-video">
+      <source src="/videos/dino.mp4" type="video/mp4" />
+        Your browser does not support the video tag guesses.
+    </video>
+    <video poster="./videos/tacos_in2.gif" playsinline muted="true" autoplay="true" loop class="featured-video">
+      <source src="/videos/shaders1.mp4" type="video/mp4" />
+        Your browser does not support the video tag guesses.
+    </video>
+    <video poster="./videos/tacos_in2.gif" playsinline muted="true" autoplay="true" loop class="featured-video">
+      <source src="/videos/vrfilterness.mp4" type="video/mp4" />
+        Your browser does not support the video tag guesses.
+    </video>
+    <video poster="./videos/tacos_in2.gif" playsinline muted="true" autoplay="true" loop class="featured-video">
+      <source src="/videos/vrmultihands.mp4" type="video/mp4" />
+        Your browser does not support the video tag guesses.
+    </video>
+    <video poster="./videos/tacos_in2.gif" playsinline muted="true" autoplay="true" loop class="featured-video">
+      <source src="/videos/circles_a.mp4" type="video/mp4" />
+        Your browser does not support the video tag guesses.
+    </video>
+
+  </section>
 </article>
