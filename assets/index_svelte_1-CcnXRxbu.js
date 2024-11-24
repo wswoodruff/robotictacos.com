c4e9ef68768a0f7df3472f22954d4a2630fd5ded
tripdragon <nickitdesign@gmail.com>
2024-11-24 15:21:56 -0500

    contact form

---
+++ assets/index_svelte_1-CcnXRxbu.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill-B5Qt9EMX.js";/* empty css                                  *//* empty css                   */import{l as o,t as m,A as r}from"./legacy-B2A5nJXN.js";var s=m('<h1 class="svelte-1hzsr80"></h1>');function n(a){let e="Howdy from Tacos!!!";var t=s();t.textContent=e,o(a,t)}r(n,{target:document.getElementById("aaa")});
